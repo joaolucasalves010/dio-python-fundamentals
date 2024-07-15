194eed3009c2427a6255ab1849511c399c04cf98
João Lucas <ylucasslalves@gmail.com>
2024-07-15 18:12:18 -0300

    Fundamentos

---
+++ 01 - Fundamentos/variaveis_constantes.py
@@ -0,0 +1,13 @@
+nome = 'Guilherme'
+idade = 27
+
+nome = 'Giovana'
+
+print(nome, idade)
+
+BRAZILIAN_STATES = ['Ba', 'Sp', 'Rj']
+
+print(BRAZILIAN_STATES)
+
+for state in BRAZILIAN_STATES:
+  print(state + " - Brasil")
