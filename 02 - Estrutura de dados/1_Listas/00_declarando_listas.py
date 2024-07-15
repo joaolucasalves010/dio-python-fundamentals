1407bf839a763f843317acc80f3db99de3d651b3
João Lucas <ylucasslalves@gmail.com>
2024-07-15 18:31:45 -0300

    listas

---
+++ 02 - Estrutura de dados/1_Listas/00_declarando_listas.py
@@ -0,0 +1,21 @@
+frutas = ['maça', 'pera', 'banana', 'uva']
+
+letras = list("python")
+
+numeros = list(range(10))
+
+carro = ['Ferrari', 'F8', 4200000, 2020, 2900, 'São Paulo', True]
+
+for dados in carro:
+  print(dados)
+
+print()
+
+for letra in letras:
+  print(letra)
+
+for numero in numeros:
+  print(numero)
+
+for fruta in frutas:
+  print(fruta)
