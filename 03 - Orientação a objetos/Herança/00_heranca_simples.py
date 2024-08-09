6d1f6b11a10061492ceba985411dac3073cb76eb
João Lucas <ylucasslalves@gmail.com>
2024-08-09 20:24:15 -0300

    Herança simples

---
+++ 03 - Orientação a objetos/Herança/00_heranca_simples.py
@@ -0,0 +1,36 @@
+class Veiculo:
+  def __init__(self, cor, placa, n_rodas):
+    self.cor = cor 
+    self.placa = placa
+    self.n_rodas = n_rodas
+
+  def ligar_motor(self):
+    print("Ligando motor")
+
+  def __str__(self):
+    return f"Cor: {self.cor}, Numero de rodas: {self.n_rodas}, Placa: {self.placa}"
+
+class Carro(Veiculo):
+  pass
+
+class Caminhao(Veiculo):
+  def __init__(self, cor, placa, n_rodas, carregado):
+    super().__init__(cor, placa, n_rodas)
+    self.carregado = carregado
+    
+  def esta_carregado(self):
+    print(f"Sim" if self.carregado else "Não estou carregado") 
+
+class Motocicleta(Veiculo):
+  pass
+
+
+
+
+moto = Motocicleta("Vermelha", "NZQ3048", 2)
+moto.ligar_motor()
+print(moto)
+caminhao = Caminhao("Preto", 'AQD1023', 8, False)
+caminhao.ligar_motor()
+caminhao.esta_carregado()
+print(caminhao)
