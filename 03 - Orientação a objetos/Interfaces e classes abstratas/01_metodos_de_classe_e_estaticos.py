13e3dc7840b77013e384eff26f1154d55610e32f
João Lucas <ylucasslalves@gmail.com>
2024-08-21 17:48:36 -0300

    metodos de class e estaticos

---
+++ 03 - Orientação a objetos/Interfaces e classes abstratas/01_metodos_de_classe_e_estaticos.py
@@ -0,0 +1,25 @@
+class Pessoa:
+  def __init__(self, nome=None, idade=None):
+    self.nome = nome
+    self.idade = idade
+
+  @classmethod
+  def criar_de_data_nascimento(cls, ano, mes, dia, nome):
+    idade = 2024 - ano
+    return Pessoa(nome, idade)
+
+  @staticmethod
+  def maior_idade(idade):
+    if idade >= 18:
+      return True
+    else:
+      return False
+
+p1 = Pessoa("João", 16)
+print(p1.idade, p1.nome)
+
+p2 = Pessoa.criar_de_data_nascimento(2008, 5, 14, "João")
+print(p2.idade, p2.nome)
+
+print(Pessoa.maior_idade(18))
+print(Pessoa.maior_idade(8))
